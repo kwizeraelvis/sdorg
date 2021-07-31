4bd45990de39cd1df9a4646fac160af584f20cb8
Kwizera Aime Elvis <kwizeraelvis6@gmail.com>
2021-07-31 13:37:06 +0200

    Intial Commit

---
+++ src/main/java/com/springapps/sdorg/SdorgApplication.java
@@ -0,0 +1,13 @@
+package com.springapps.sdorg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdorgApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SdorgApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/springapps/sdorg/SdorgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springapps.sdorg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdorgApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
