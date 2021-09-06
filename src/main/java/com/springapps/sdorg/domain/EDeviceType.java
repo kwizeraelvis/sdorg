034728788e04bde91234074834f6d0058bbac469
Kwizera Aime Elvis <kwizeraelvis6@gmail.com>
2021-09-06 14:58:28 +0200

    Add device type enumerator

---
+++ src/main/java/com/springapps/sdorg/domain/EDeviceType.java
@@ -0,0 +1,6 @@
+package com.springapps.sdorg.domain;
+
+public enum EDeviceType {
+    DESKTOP, LAPTOP, PRINTER, SCANNER, PROJECTOR,CAMERA, MOBILE_PHONE,
+    LAND_LINE, TABLET, ROUTER, SWITCH, ACCESS_POINT, OTHER;
+}
